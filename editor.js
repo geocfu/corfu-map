06344a68a795df84e644cf7688787d82695abf0d
geocfu <geocfuone@gmail.com>
2018-02-16 17:46:04 +0200

    Indentation fixes

--- editor.js
+++ editor.js
@@ -13,41 +13,41 @@ function init() {
         streetViewControl: false,
         fullscreenControl: false,
         mapTypeId: 'satellite'
-});
-	
-  map.data.setControls(['Point', 'LineString', 'Polygon']);
-  map.data.setStyle({
-    editable: true,
-    draggable: true,
-    clickable: true
-  });
+    });
 
-  bindDataLayerListeners(map.data);
+    map.data.setControls(['Point', 'LineString', 'Polygon']);
+    map.data.setStyle({
+        editable: true,
+        draggable: true,
+        clickable: true
+    });
 
-  // Retrieve HTML elements.
-  var mapContainer = document.getElementById('map-holder');
-  geoJsonOutput = document.getElementById('geojson-output');
-  downloadLink = document.getElementById('download-link');
+    bindDataLayerListeners(map.data);
+
+    // Retrieve HTML elements.
+    var mapContainer = document.getElementById('map-holder');
+    geoJsonOutput = document.getElementById('geojson-output');
+    downloadLink = document.getElementById('download-link');
 }
 
 google.maps.event.addDomListener(window, 'load', init);
 
 // Refresh different components from other components.
 function refreshGeoJsonFromData() {
-  map.data.toGeoJson(function(geoJson) {
-    geoJsonOutput.value = JSON.stringify(geoJson);
-    refreshDownloadLinkFromGeoJson();
-  });
+    map.data.toGeoJson(function(geoJson) {
+        geoJsonOutput.value = JSON.stringify(geoJson);
+        refreshDownloadLinkFromGeoJson();
+    });
 }
 
 // Refresh download link.
 function refreshDownloadLinkFromGeoJson() {
-  downloadLink.href = "data:;base64," + btoa(geoJsonOutput.value);
+    downloadLink.href = "data:;base64," + btoa(geoJsonOutput.value);
 }
 
 // Apply listeners to refresh the GeoJson display on a given data layer.
 function bindDataLayerListeners(dataLayer) {
-  dataLayer.addListener('addfeature', refreshGeoJsonFromData);
-  dataLayer.addListener('removefeature', refreshGeoJsonFromData);
-  dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
+    dataLayer.addListener('addfeature', refreshGeoJsonFromData);
+    dataLayer.addListener('removefeature', refreshGeoJsonFromData);
+    dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
 }
